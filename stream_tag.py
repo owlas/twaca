1245a4be078c61ea1382c234eb1459a9ee2f0f84
Oliver Laslett <user@Lasletto.local>
2015-03-27 11:03:24 +0000

    Added initial streaming function

---
+++ stream_tag.py
@@ -0,0 +1,26 @@
+"""
+Oliver W. Laslett
+O.Laslett@soton.ac.uk
+see LICENSE file
+
+
+Given keywords it streams tweets
+"""
+
+from TwitterAPI import TwitterAPI
+
+# First login
+c_key = ''
+c_sec = ''
+a_key = ''
+a_sec = ''
+
+# Login and get the api object
+api = TwitterAPI(c_key, c_sec, a_key, a_sec)
+
+# Get football tweets
+r = api.request('statuses/filter', {'track':'football,lithuania', 'filter_level':'none'})
+for i in r.get_iterator():
+    if 'text' in i:
+        print i['user']['name']
+        print i['text']
